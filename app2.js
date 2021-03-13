50a8530b9252187f5733d94e006c1cafdd93125f
themiskiller <74245699+themiskiller@users.noreply.github.com>
2021-03-14 00:03:57 +0200

    overall css redux

---
+++ app2.js
@@ -0,0 +1,17 @@
+function dis(val){
+document.getElementById("display").value += val;
+}
+
+ function solve(){
+let x = document.getElementById("display").value;
+let y = eval(x)
+document.getElementById("display").value = y;
+}
+
+function Clear(){
+var x = document.getElementById("display");
+x.value = 0;
+if (x.innerHTML !== 0) {
+      x.innerHTML = 0;  
+}
+};
